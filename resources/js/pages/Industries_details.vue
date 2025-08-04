a0e92a8d3c11b2d01f080045662ff2e0161ce1dc
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-04 12:52:42 +0500

    Industries_details page created

---
+++ resources/js/pages/Industries_details.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="page-wrapper">
+
+    <!-- Start main-content -->
+	<section class="page-title" style="background-image: url(assets/images/background/page-title.jpg);">
+		<div class="auto-container">
+			<div class="title-outer">
+				<h1 class="title">Industries Details</h1>
+				<ul class="page-breadcrumb">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li>Industries</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<!-- end main-content -->
+
+	<!--Project Details Start-->
+	<section class="project-details">
+		<div class="container">
+
+			<div class="project-details__content">
+				<div class="row">
+					<div class="col-xl-8 col-lg-8">
+						<div class="project-details__content-left">
+							<h3 class="mb-4">We have data bank of skilled, semi-skilled and unskilled workforce for the below industries.
+                            </h3>
+							<ul class="list-unstyled contact-details__info">
+							     <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Buildings and Civil Construction</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Oil & Gas (Petrochemical and Heavy Engineering)</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Infrastructure</h6>
+							        </div>
+							    </li>
+
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Light Duty Manufacturing</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Healthcare</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Agriculture & Farming</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>FMCG (Courier & Food Delivery)</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Facilities Management</h6>
+							        </div>
+							    </li>
+							    <li>
+							        <div class="icon">
+							            <span class="lnr-icon-location"></span>
+
+							        </div>
+							        <div class="text">
+							            <h6>Temporary Staffing /Manpower Leasing (Romania)
+                                        </h6>
+							        </div>
+							    </li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+	</section>
+	<!--Project Details End-->
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Industries_details'
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 import Introduction from '../pages/Introduction.vue'
+import Industries_details from '../pages/Industries_details.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/about', name: 'about', component: About },
-  { path: '/introduction', name: 'introduction', component: Introduction }
+  { path: '/introduction', name: 'introduction', component: Introduction },
+  { path: '/industries_details', name: 'industries_details', component: Industries_details },
 ]
 
 const router = createRouter({
