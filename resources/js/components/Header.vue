0fd69c512bc0c59481ae75cc16cf59cb3cf816bf
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-04 12:31:24 +0500

    Header Fixed

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,163 +1,213 @@
 <template>
-  <header class="main-header header-style-four">
-    <!-- Main box -->
-    <div class="main-box">
-      <div class="logo-box">
-        <div class="logo">
-          <a href="/">
-            <img src="/assets/images/dhothar_logo.png" alt="" title="Tronis" />
-          </a>
+    <header class="main-header header-style-four">
+        <!-- Main box -->
+        <div class="main-box">
+            <div class="logo-box">
+                <div class="logo">
+                    <a href="/">
+                        <img src="/assets/images/dhothar_logo.png" alt="" title="Tronis" />
+                    </a>
+                </div>
+            </div>
+
+            <!--Nav Box-->
+            <div class="nav-outer">
+                <nav class="nav main-menu">
+                    <ul class="navigation">
+                        <li class="current"><a href="/">Home</a></li>
+
+                        <li class="dropdown">
+                            <a href="#">About</a>
+                            <ul>
+                                <li><a href="/introduction">Introduction</a></li>
+                                <li><a href="/industries_details">Industries Details</a></li>
+                                <li><a href="/our_services">Our Services</a></li>
+                                <li><a href="/mission">Mission</a></li>
+                                <li><a href="/vision">Vision</a></li>
+                                <li><a href="/values">Values</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="/introduction">Introduction</a>
+                            <ul>
+                                <li><a href="/why_dhothar">Why Dhothar</a></li>
+                                <li><a href="/company_organizational_chart">Company Organizational Chart</a></li>
+                                <li><a href="/company_offices">Company Offices</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#">Recruitment Chart</a>
+                            <ul>
+                                <li><a href="/europe_process">Europe</a></li>
+                                <li><a href="/kingdom_saudi">Kingdom of Saudi Arabia</a></li>
+                                <li><a href="/uae_qatar_etc">U.A.E, QATAR, KUWAIT, OMAN & BAHRAIN</a></li>
+                                <li><a href="/sourcing_matrix">Our Sourcing Matrix</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="/terms_conditions">Terms</a>
+                            <ul>
+                                <li><a href="/terms_conditions">Terms and Conditions</a></li>
+                                <li><a href="/orientation">Orientation</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="/japan_introduction">
+                                <img src="/assets/images/dhothar_images/jp-flag.jpg" width="35"
+                                    style="vertical-align: middle;" />
+                                &nbsp;&nbsp; JAPAN
+                            </a>
+                            <ul>
+                                <li><a href="/japan_introduction">Introduction</a></li>
+                                <li><a href="/titp_japan">TITP</a></li>
+                                <li><a href="/sswp">Specified Skilled Worker</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="/new_jobs">New Jobs</a></li>
+                        <li><a href="/gallery">Gallery</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+
         </div>
-      </div>
 
-      <!--Nav Box-->
-      <div class="nav-outer">
-        <nav class="nav main-menu">
-          <ul class="navigation">
-            <li class="current"><a href="/">Home</a></li>
-
-            <li class="dropdown">
-              <a href="#">About</a>
-              <ul>
-                <li><a href="/introduction">Introduction</a></li>
-                <li><a href="/industries_details">Industries Details</a></li>
-                <li><a href="/our_services">Our Services</a></li>
-                <li><a href="/mission">Mission</a></li>
-                <li><a href="/vision">Vision</a></li>
-                <li><a href="/values">Values</a></li>
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a href="/introduction">Introduction</a>
-              <ul>
-                <li><a href="/why_dhothar">Why Dhothar</a></li>
-                <li><a href="/company_organizational_chart">Company Organizational Chart</a></li>
-                <li><a href="/company_offices">Company Offices</a></li>
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a href="#">Recruitment Chart</a>
-              <ul>
-                <li><a href="/europe_process">Europe</a></li>
-                <li><a href="/kingdom_saudi">Kingdom of Saudi Arabia</a></li>
-                <li><a href="/uae_qatar_etc">U.A.E, QATAR, KUWAIT, OMAN & BAHRAIN</a></li>
-                <li><a href="/sourcing_matrix">Our Sourcing Matrix</a></li>
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a href="/terms_conditions">Terms</a>
-              <ul>
-                <li><a href="/terms_conditions">Terms and Conditions</a></li>
-                <li><a href="/orientation">Orientation</a></li>
-              </ul>
-            </li>
-
-            <li class="dropdown">
-              <a href="/japan_introduction">
-                <img src="/assets/images/dhothar_images/jp-flag.jpg" width="35" style="vertical-align: middle;" />
-                &nbsp;&nbsp; JAPAN
-              </a>
-              <ul>
-                <li><a href="/japan_introduction">Introduction</a></li>
-                <li><a href="/titp_japan">TITP</a></li>
-                <li><a href="/sswp">Specified Skilled Worker</a></li>
-              </ul>
-            </li>
-
-            <li><a href="/new_jobs">New Jobs</a></li>
-            <li><a href="/gallery">Gallery</a></li>
-          </ul>
-        </nav>
-      </div>
-
-      <!-- Outer Box -->
-      <div class="outer-box">
-        <!-- Header Search -->
-        <button class="ui-btn ui-btn search-btn">
-          <span class="icon lnr lnr-icon-search"></span>
-        </button>
-        <a href="#" class="theme-btn btn-style-one dark-bg"><span class="btn-title">Get a quote</span></a>
-        <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
-      </div>
-    </div>
-
-    <!-- Mobile Menu -->
-    <div class="mobile-menu">
-      <div class="menu-backdrop"></div>
-      <nav class="menu-box">
-        <div class="upper-box">
-          <div class="nav-logo">
-            <a href="/">
-              <img src="/assets/images/dhothar_logo.png" alt="" title="" />
-            </a>
-          </div>
-          <div class="close-btn"><i class="icon fa fa-times"></i></div>
-        </div>
-        <ul class="navigation clearfix"></ul>
-        <ul class="contact-list-one">
-          <li>
-            <div class="contact-info-box">
-              <i class="icon lnr-icon-phone-handset"></i>
-              <span class="title">Call Now</span>
-              <a href="tel:+92880098670">Pak +92514848692<br />Romanian +40739916997<br />UAE +971501005758</a>
-            </div>
-          </li>
-          <li>
-            <div class="contact-info-box">
-              <span class="icon lnr-icon-envelope1"></span>
-              <span class="title">Send Email</span>
-              <a href="mailto:info@dhotharinternational.com">info@dhotharinternational.com</a>
-            </div>
-          </li>
-          <li>
-            <div class="contact-info-box">
-              <span class="icon lnr-icon-clock"></span>
-              <span class="title">Opening Hours</span>
-              Mon - Sat 8:00 - 6:30, Sunday - CLOSED
-            </div>
-          </li>
-        </ul>
-        <ul class="social-links">
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-        </ul>
-      </nav>
-    </div>
-
-    <!-- Sticky Header -->
-    <div class="sticky-header">
-      <div class="auto-container">
-        <div class="inner-container">
-          <div class="logo">
-            <a href="/" title="">
-              <img src="/assets/images/dhothar_logo.png" alt="" title="" />
-            </a>
-          </div>
-          <div class="nav-outer">
-            <nav class="main-menu">
-              <div class="navbar-collapse show collapse clearfix">
-                <ul class="navigation clearfix">
-                  <li><div id="google_element"></div></li>
+        <!-- Mobile Menu -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <nav class="menu-box">
+                <div class="upper-box">
+                    <div class="nav-logo">
+                        <a href="/">
+                            <img src="/assets/images/dhothar_logo.png" alt="" title="" />
+                        </a>
+                    </div>
+                    <div class="close-btn"><i class="icon fa fa-times"></i></div>
+                </div>
+                <ul class="navigation clearfix"></ul>
+                <ul class="contact-list-one">
+                    <li>
+                        <div class="contact-info-box">
+                            <i class="icon lnr-icon-phone-handset"></i>
+                            <span class="title">Call Now</span>
+                            <a href="tel:+92880098670">Pak +92514848692<br />Romanian +40739916997<br />UAE
+                                +971501005758</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="contact-info-box">
+                            <span class="icon lnr-icon-envelope1"></span>
+                            <span class="title">Send Email</span>
+                            <a href="mailto:info@dhotharinternational.com">info@dhotharinternational.com</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="contact-info-box">
+                            <span class="icon lnr-icon-clock"></span>
+                            <span class="title">Opening Hours</span>
+                            Mon - Sat 8:00 - 6:30, Sunday - CLOSED
+                        </div>
+                    </li>
                 </ul>
-              </div>
+                <ul class="social-links">
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
             </nav>
-            <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
-          </div>
         </div>
-      </div>
-    </div>
-  </header>
+
+        <!-- Sticky Header -->
+        <div class="sticky-header">
+            <div class="auto-container">
+                <div class="inner-container">
+                    <div class="logo">
+                        <a href="/" title="">
+                            <img src="/assets/images/dhothar_logo.png" alt="" title="" />
+                        </a>
+                    </div>
+                    <div class="nav-outer">
+                        <nav class="main-menu">
+                            <div class="navbar-collapse show collapse clearfix">
+                                <ul class="navigation clearfix">
+                                    <li class="current"><a href="/">Home</a></li>
+
+                                    <li class="dropdown">
+                                        <a href="#">About</a>
+                                        <ul>
+                                            <li><a href="/introduction">Introduction</a></li>
+                                            <li><a href="/industries_details">Industries Details</a></li>
+                                            <li><a href="/our_services">Our Services</a></li>
+                                            <li><a href="/mission">Mission</a></li>
+                                            <li><a href="/vision">Vision</a></li>
+                                            <li><a href="/values">Values</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="dropdown">
+                                        <a href="/introduction">Introduction</a>
+                                        <ul>
+                                            <li><a href="/why_dhothar">Why Dhothar</a></li>
+                                            <li><a href="/company_organizational_chart">Company Organizational Chart</a>
+                                            </li>
+                                            <li><a href="/company_offices">Company Offices</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="dropdown">
+                                        <a href="#">Recruitment Chart</a>
+                                        <ul>
+                                            <li><a href="/europe_process">Europe</a></li>
+                                            <li><a href="/kingdom_saudi">Kingdom of Saudi Arabia</a></li>
+                                            <li><a href="/uae_qatar_etc">U.A.E, QATAR, KUWAIT, OMAN & BAHRAIN</a></li>
+                                            <li><a href="/sourcing_matrix">Our Sourcing Matrix</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="dropdown">
+                                        <a href="/terms_conditions">Terms</a>
+                                        <ul>
+                                            <li><a href="/terms_conditions">Terms and Conditions</a></li>
+                                            <li><a href="/orientation">Orientation</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="dropdown">
+                                        <a href="/japan_introduction">
+                                            <img src="/assets/images/dhothar_images/jp-flag.jpg" width="35"
+                                                style="vertical-align: middle;" />
+                                            &nbsp;&nbsp; JAPAN
+                                        </a>
+                                        <ul>
+                                            <li><a href="/japan_introduction">Introduction</a></li>
+                                            <li><a href="/titp_japan">TITP</a></li>
+                                            <li><a href="/sswp">Specified Skilled Worker</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li><a href="/new_jobs">New Jobs</a></li>
+                                    <li><a href="/gallery">Gallery</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 
 <script>
 export default {
-  name: "Header",
+    name: "Header",
 };
 </script>
 
