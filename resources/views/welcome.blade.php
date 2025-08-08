4ae9e7199e0b5f41c7d1e5d0c4748f80f557dcc3
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-08 17:51:17 +0500

    call assets like laravel import

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dhothar International </title>
+    <title>Dhothar International</title>
+
     <!-- Stylesheets -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
-
-    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
 
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
     <meta name="description" content="Dhothar International Recruitment Agency: Your gateway to global career opportunities. Explore overseas employment in Romania, Qatar, Saudi Arabia, the UAE, Oman, Bahrain, and Albania. Our expert team connects skilled professionals with top-tier positions in dynamic markets. Unlock your potential with Dhothar – Where Careers Know No Boundaries.">
     <meta name="keywords" content="Dhothar International Recruitment Agency, Dhothar International, Overseas Employment, Work Permit Visa in Romania, Qatar, Saudi Arabia, the UAE, Oman, Bahrain, and Albania.">
@@ -22,60 +21,60 @@
     <link rel="canonical" href="https://dhotharinternational.com/">
 
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <!-- Responsive -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
     <style>
-        banner-section-three{
+        banner-section-three {
             position: absolute;
-        left: 0;
-        top: 0;
-        height: 100%;
-        width: 100%;
-        background: #131313;
-        opacity: 0.6;
-        content: "";
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            background: #131313;
+            opacity: 0.6;
+            content: "";
         }
         .slide-item {
-        position: relative;
-    }
-
-    .bg-image::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5); /* Adjust the last value for the opacity of the black overlay */
-        z-index: 1;
-    }
-
-    .auto-container {
-        position: relative;
-        z-index: 2;
-    }
+            position: relative;
+        }
+        .bg-image::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 1;
+        }
+        .auto-container {
+            position: relative;
+            z-index: 2;
+        }
     </style>
-    <!-- Styles / Scripts -->
+
+    <!-- Laravel Vite -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 </head>
 <body>
     <div id="app"></div>
-    <!-- Scroll To Top -->
-    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
 
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/jquery.fancybox.js"></script>
-<script src="assets/js/wow.js"></script>
-<script src="assets/js/appear.js"></script>
-<script src="assets/js/isotope.js"></script>
-<script src="assets/js/select2.min.js"></script>
-<script src="assets/js/owl.js"></script>
-<script src="assets/js/script.js"></script>
+    <!-- Scroll To Top -->
+    <div class="scroll-to-top scroll-to-target" data-target="html">
+        <span class="fa fa-angle-up"></span>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.fancybox.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.js') }}"></script>
+    <script src="{{ asset('assets/js/appear.js') }}"></script>
+    <script src="{{ asset('assets/js/isotope.js') }}"></script>
+    <script src="{{ asset('assets/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 </html>
