44e45c83ac8fa0f4b98d1b0810fd06a3b459f9e1
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2025-08-08 14:54:16 +0500

    Slides added

--- resources/js/components/BannerSection.vue
+++ resources/js/components/BannerSection.vue
@@ -3,11 +3,11 @@
     <div class="banner-carousel owl-carousel owl-theme default-navs">
       <!-- Slide Item -->
       <div class="slide-item">
-        <div class="bg-image" style="background-image: url(assets/images/main-slider/5.jpg);"></div>
+        <div class="bg-image" style="background-image: url(assets/images/main-slider/develop.png);"></div>
         <div class="auto-container">
           <div class="content-box">
-            <span class="sub-title animate-1">Smart Digital Marketing Agency</span>
-            <h1 class="title animate-2">Shaping the <br>Perfect Website <br>Solutions.</h1>
+            <span class="sub-title animate-1">Buildings and Civil Construction</span>
+            <h1 class="title animate-2">Buildings   <br> and Civil Construction.</h1>
             <div class="btn-box animate-3">
               <a href="page-about.html" class="theme-btn btn-style-one hover-light">
                 <span class="btn-title">Discover more</span>
@@ -19,11 +19,11 @@
 
       <!-- Slide Item -->
       <div class="slide-item">
-        <div class="bg-image" style="background-image: url(assets/images/main-slider/4.jpg);"></div>
+        <div class="bg-image" style="background-image: url(assets/images/main-slider/oil.png);"></div>
         <div class="auto-container">
           <div class="content-box">
-            <span class="sub-title animate-1">Smart Digital Marketing Agency</span>
-            <h1 class="title animate-2">Shaping the <br>Perfect Website <br>Solutions.</h1>
+            <span class="sub-title animate-1">Oil & Gas</span>
+            <h1 class="title animate-2">Oil & Gas <br> (Petrochemical and Heavy Engineering)</h1>
             <div class="btn-box animate-3">
               <a href="page-about.html" class="theme-btn btn-style-one hover-light">
                 <span class="btn-title">Discover more</span>
@@ -32,6 +32,52 @@
           </div>
         </div>
       </div>
+      <!-- Slide Item -->
+      <div class="slide-item">
+        <div class="bg-image" style="background-image: url(assets/images/main-slider/infra.png);"></div>
+        <div class="auto-container">
+          <div class="content-box">
+            <span class="sub-title animate-1">Infrastructure</span>
+            <h1 class="title animate-2">Infrastructure.</h1>
+            <div class="btn-box animate-3">
+              <a href="page-about.html" class="theme-btn btn-style-one hover-light">
+                <span class="btn-title">Discover more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide Item -->
+      <div class="slide-item">
+        <div class="bg-image" style="background-image: url(assets/images/main-slider/light.png);"></div>
+        <div class="auto-container">
+          <div class="content-box">
+            <span class="sub-title animate-1">Light Duty Manufacturing</span>
+            <h1 class="title animate-2">Light <br> Duty Manufacturing.</h1>
+            <div class="btn-box animate-3">
+              <a href="page-about.html" class="theme-btn btn-style-one hover-light">
+                <span class="btn-title">Discover more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide Item -->
+      <div class="slide-item">
+        <div class="bg-image" style="background-image: url(assets/images/main-slider/agri.png);"></div>
+        <div class="auto-container">
+          <div class="content-box">
+            <span class="sub-title animate-1">Agriculture And Farming</span>
+            <h1 class="title animate-2">Agriculture <br> And Farming.</h1>
+            <div class="btn-box animate-3">
+              <a href="page-about.html" class="theme-btn btn-style-one hover-light">
+                <span class="btn-title">Discover more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </section>
 </template>
